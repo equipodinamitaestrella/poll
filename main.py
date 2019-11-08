ff44ff1896abea41df75bffdeaa6181cf6dac2e0
ktsb3 <keshava.t.s.b@hotmail.com>
2019-11-08 11:58:36 -0600

    Created person class

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 """
-main.py solve the n-body problem using newton
+main.py Define person class to poll
 Copyright (C) 2019 Jorge Antonio Camarena Pliego (camarenapliego@gmail.com)
 Keshava Tonathiu Sanchez Barbosa (keshava.t.s.b@gmail.com)
 Stephany Dzoara Vargas Mier (stephanydvm@comunidad.unam.mx)
@@ -17,3 +17,16 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>."""
 
+class person:
+    def __init__(self, age = 18, sex, income = 1825, education = 12, race, religion, 
+                marital_status, sexual_orientation, children = 0, security_percetion = 10):
+        self.age = age #years
+        self.sex = sex #M, F, I
+        self.income = income #dollars per year
+        self.education = education #years
+        self.race = race #
+        self.religion = religion #
+        self.marital_status = marital_status
+        self.sexual_orientation = sexual_orientation
+        self.children = children
+        self.security_percetion = security_percetion
